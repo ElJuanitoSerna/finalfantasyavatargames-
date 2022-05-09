7407bd5c6aca8857a3ea0301c39d1d84dde90c1c
ElJuanito <juan.serna@sjsu.edu>
2022-05-09 10:15:57 -0700

    edits

--- 592022sketch.js
+++ 592022sketch.js
@@ -2,36 +2,12 @@
 
 
 
-let state = 'title';
+let gamestate = 'title';
 //let state = 'gameScreen';
 //let state = 'end';
-let roll = ['You'];
 
-let action = ['grab a', 'throw a', 'jump over a', 'run into a', 'pick up a', 'conjure a', 'walk away from a', 'duck a', 'use a', 'swing a'];
-
-let weapon = ['axe', 'potion bottle', 'long sword', 'warhammer', 'drink of elixir', 'crossbow', 'morning star', ' short sword', 'gaunlet', 'helmet'];
-
-let actiont = [',  and throw it at', ', and run into', ', and hide from', ', attack', ', cast a spell at', ',and vanquish ', ',it misses ', ', it is blocked by']
-
-
-let creature = ['a troll', 'a gargoyle', 'a serpant', 'a witch', 'a dark elf', 'a sorcerer', 'a fairy', 'a cave gnome', 'a barbarian', 'a fighter', 'a paladin', 'a ranger'];
-
-
-
-
-let dr;
-let ac;
-let wp;
-let aet;
-let cr;
-let myFont;
-let wrds;
-let button;
-let addMoreButton;
-let centerPicture;
 let cnv;
-
-//let firstPage;
+let firstPage;
 let secondPage;
 var canvas;
 var click1;
@@ -93,7 +69,6 @@ var backGroundHtml;
 
 
 
-
 function preload() {
   firstPage = loadImage('firstg1.png');
   secondPage = loadImage('game.JPG');
@@ -122,12 +97,13 @@ function preload() {
   //cursorImg = loadImage("Assets/brushtwo.png")
   backGroundT = loadImage("Assets/backgroundtrans.png")
   backGroundHtml = loadImage("Assets/backgroundhtml.png")
-  myFont = loadFont('Assets/Yatra_One/YatraOne-Regular.ttf');
   //myFont = loadFont ('Assets/Yatara-One-Regular.ttf');
 }
 
 function setup() {
   cnv = createCanvas(1400, 800);
+  //canvas.parent('sketch-holder');
+ //frameRate(60);
 
   //let btn1 = text(input.value(), 160, 85)
   //btn1.position(x, y)
@@ -139,7 +115,6 @@ function setup() {
   input3 = createInput(" age")
   input4 = createInput(" weight")
   input5 = createInput(" height")
-
   //createP();
   //slider = createSlider(1, 20, 10);
   //eraser = createButton("clear");
@@ -386,28 +361,11 @@ function setup() {
   leftBottom.onPress = function() {
     avatarBottom = bottomLeft
   };
-
-    fill('255');
-    textFont(myFont);
-    textSize(25);
-    cnv = createCanvas(1000, 1000);
-    //cnv.parent("#canvasDiv");
-
-    background(66, 40, 14);
-    frameRate(60);
-    intText();
-    //button = createButton("Click to start your next Adventure")
-    button = select('#randButton')
-    button.mousePressed(buttonPressed);
-    button.class("randomizerButton");
-    //addMoreButton.mousePresses(buttonPressed);
-    //
-    for(let i =0; i <3; i++){
 }
 
 
 function draw() {
-  switch (state) {
+  switch (gamestate) {
     case 'title':
       title();
       cnv.mouseClicked(titleMouseClicked);
@@ -429,41 +387,7 @@ function draw() {
     default:
       break;
   }
-  loadImage('ASSETS/background/Tavernbackground.jpeg', img => {
-    image(img, width * 0.2, width * 0.26, width * 0.6, width * 0.4, );
-  });
-  function buttonPressed() {
 
-    dr = int(random(roll.length));
-    ac = int(random(action.length));
-    wp = int(random(weapon.length));
-    at = int(random(actiont.length));
-    cr = int(random(creature.length));
-
-    wrds = roll[dr] + '   ' + action[ac] + ' ' + weapon[wp] + ' ' + actiont[at] + ' ' + creature[cr] + '.';
-
-
-    fortText();
-    console.log(wrds);
-
-  }
-  function intText() {
-    fill(247, 255, 0);
-    textAlign(CENTER);
-
-    text('You walk out of a Tavern, click to see what happens next?', width * 0.50, height * 0.16);
-  }
-
-
-
-  function fortText() {
-    background(66, 40, 14);
-    intText();
-    fill(247, 255, 0);
-    textAlign(CENTER);
-    text('Your next adventure begins', width * 0.5, height * 0.75);
-    text(wrds, width * 0.5, height * 0.85)
-  }
 
 }
 
@@ -484,43 +408,9 @@ function title() {
 
 function titleMouseClicked() {
   console.log('canvas is clicked on title page');
-  state = 'instructions'
-
-}
-function instructions() {
-  textSize(40);
-  stroke(255);
- background (2);
-
-
-  text('instructions', 400, 100);
-  textSize(30);
-  //image(fisrtTitleScreen, 80, 100, 1240, 600);
-}
-
-function titleMouseClicked() {
-  console.log('canvas is clicked on title page');
   state = 'level 1'
 
 }
-
-function title() {
-  textSize(40);
-  stroke(255);
- background (2);
-
-
-  //text('CLICK YOUR ADVENTURE AVATAR BUILDING GAME', 400, 100);
-  textSize(30);
-  image(fisrtTitleScreen, 80, 100, 1240, 600);
-}
-
-function titleMouseClicked() {
-  console.log('canvas is clicked on title page');
-  state = 'level 1'
-
-}
-
 
 function level1() {
   secondPage.image = secondPage;
@@ -537,8 +427,6 @@ function level1MouseClicked() {
   state = 'title'
 }
 
-
-
 function end() {
   background(2);
 
@@ -554,6 +442,26 @@ function endMouseClicked() {
   console.log('canvas is clicked on end');
 }
 
+//function mouseClicked() {
+//if (mouseX > 400) {
+//c = get(mouseX, mouseY);
+//checkbox.checked(false);
+//} else {
+//stampRectangle(c);
+//}
+//}
+
+//function mouseDragged() {
+//if (checkbox.checked()) {
+//stroke(255);
+//} else {
+//stroke(c)
+//}
+//if (mouseX < 390) {
+//strokeWeight(slider.value());
+//line(mouseX, mouseY, pmouseX, pmouseY);
+//}
+//}
 
 function changeBG() {
   background(255);
@@ -586,4 +494,7 @@ function gameScreen() {
   centerMiddleFace.draw();
   headMiddle.draw();
   //image(backGroundT,200, 200,180,180)
+
+
+
 }
